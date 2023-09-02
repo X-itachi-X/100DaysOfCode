b4f5866d3b422f64a8d2352fa692439043e1ae00
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-09-02 08:56:16 +0000

    Sep 2, 2023, 2:26 PM

--- day19-26/Basic JavaScript/002-declare-javascript-variables.js
+++ day19-26/Basic JavaScript/002-declare-javascript-variables.js
@@ -1,1 +1,4 @@
 var ourName;
+/*In computer science, data is anything that is meaningful to the computer. 
+JavaScript provides eight different data types which are 
+undefined, null, boolean, string, symbol, bigint, number, and object.*/
