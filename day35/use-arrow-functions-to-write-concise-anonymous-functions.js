287c88736dba6f22ed35a322bdd4b0dae4f0537d
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-09-14 01:02:01 +0000

    Sep 14, 2023, 6:32 AM

---
+++ day35/use-arrow-functions-to-write-concise-anonymous-functions.js
@@ -0,0 +1,14 @@
+//Anonymous Function
+const myFunc = () => {
+    const myVar = "value";
+    return myVar;
+  }
+  
+  //Arrow Functions to Write Concise Anonymous Functions
+  
+  const myFunc1 = () => {
+    const myVar = "value";
+    return myVar;
+  }
+  
+  const myFunc2 = () => "value";
